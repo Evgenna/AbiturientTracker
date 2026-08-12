5f1919aa0d470d7fdecb98c1c33d511c8227018a
evgeniia <evgennawzzc@gmail.com>
2026-08-12 16:21:59 +0300

    delete: unused controller methods

--- src/Controllers/UniversityController.cs
+++ src/Controllers/UniversityController.cs
@@ -18,18 +18,6 @@ namespace University
             var majors = await _universityProxy.GetMajors();
             return Ok(majors);
         }
-        [HttpGet("abiturients")]
-        public async Task<IActionResult> GetAbiturients()
-        {
-            var abiturientList = await _universityProxy.GetAbiturients();
-            var abiturients = DistributionService.Prepare(abiturientList);
-
-            var majors = abiturientList.Select(a => a.Major).ToList();
-
-            abiturients = DistributionService.Distribute(abiturients, majors);
-
-            return Ok(abiturients);
-        }
 
         [HttpGet("rating")]
         public async Task<IActionResult> GetRating()
