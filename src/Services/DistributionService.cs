7ffb9c2372da73a1873b43e44875c7acacf203c2
evgeniia <evgennawzzc@gmail.com>
2026-08-12 12:51:53 +0300

    docs: add comments for distribution

--- src/Services/DistributionService.cs
+++ src/Services/DistributionService.cs
@@ -35,12 +35,18 @@ namespace Abiturients
             return abiturientList;
         }
 
+        /// <summary>
+        /// Распределение абитуриентов по доступным специальностям с учетом приоритетов и поданных согласий
+        /// </summary>
+        /// <param name="abiturients">Список абитуриентов</param>
+        /// <param name="majorDetails">Список специальностей</param>
+        /// <returns>Список абитуриентов с назначенными специальностями</returns>
         public static List<Abiturient> Distribute(List<Abiturient> abiturients, List<MajorDetails> majorDetails)
         {
-            var abiturientList = new List<Abiturient>(abiturients);
+            var abiturientList = abiturients.Select(a => new Abiturient(a)).ToList();
 
-            var takingPlaces = new Dictionary<string, int>();
-            var agreementPlaces = new Dictionary<string, int>();
+            var takingPlaces = new Dictionary<string, int>(); // Места на основной высший приоритет
+            var agreementPlaces = new Dictionary<string, int>(); // Места на высший проходной приоритет
             foreach (MajorDetails major in majorDetails)
             {
                 takingPlaces[major.Id] = 0;
@@ -49,8 +55,10 @@ namespace Abiturients
             foreach (var abiturient in abiturientList)
             {
                 abiturient.MajorPriorities = [.. abiturient.MajorPriorities.OrderBy(a => a.Priority)];
-                bool currentPlace = false;
-                bool agreementPlace = false;
+
+                bool currentPlace = false; // Занял ли абитуриент место в основном высшем приоритете
+                bool agreementPlace = false; // Занял ли абитуриент место в высшем проходном приоритете
+
                 foreach (var priority in abiturient.MajorPriorities)
                 {
                     var majorId = priority.Id;
@@ -61,22 +69,26 @@ namespace Abiturients
 
                     var majorPlaces = major.Places;
 
+                    // Если места по обоим приоритетам заняты, то переходим к следующему приоритету
+                    if (takingPlaces[majorId] >= majorPlaces && agreementPlaces[majorId] >= majorPlaces) continue;
 
-                    if (takingPlaces[majorId] >= majorPlaces && agreementPlaces[majorId] >= majorPlaces) break;
+                    // Если у абитуриента определены оба приоритета, то дальнейшие приоритеты не рассматриваются
+                    if (agreementPlace && currentPlace) break;
 
+                    // Распределение по основному высшему приоритету
                     if (takingPlaces[majorId] < majorPlaces && !currentPlace)
                     {
                         currentPlace = true;
                         takingPlaces[majorId]++;
                         abiturient.CurrentMajor = priority.Name;
                     }
+                    // Распределение по высшему проходному приоритету
                     if (agreementPlaces[majorId] < majorPlaces && !agreementPlace && abiturient.HasAgreement)
                     {
                         agreementPlace = true;
                         agreementPlaces[majorId]++;
                         abiturient.AgreementMajor = priority.Name;
                     }
-                    if (agreementPlace && currentPlace) break;
                 }
             }
             return abiturientList;
